0616e8b6bab7a11886b8d474012cd4bd252c4421
Louise McCafferty <mccafferty_@hotmail.com>
2022-10-20 09:56:19 +0100

    final commit

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -7,59 +7,36 @@ const MainContainer = () => {
     useEffect(()=> {
         fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
         .then(res => res.json())
-        .then(
-                id => getTitleById(id)
-            )
-    }, [])
-
-//     const handleSelectChange = ( character ) => {
-//         const episodePromises = character.episode.map((episode) => {
-//             return fetch (episode).then(res => res.json());
-//         })
-//         Promise.all(episodePromises)
-//         .then((data) => {
-//             setEpisodes(data);
-//             setSelectedCharacter(character);
-//         })
-    
-//         setSelectedCharacter( character );
-//     }
-
-//     return (
-//         <div>
-//             <h1>Characters</h1>
-//             <CharacterSelect characters={ characters } handleSelectChange={ handleSelectChange } />
-//             { selectedCharacter ? <CharacterDetail character={ selectedCharacter } episodes ={episodes}/> : null }
-//         </div>
-//     )
-// }
-
-// export default MainContainer
-
-
-    
-    const getTitleById = ( storyId ) => {
-        const storyPromises = (storyId) => {
-            return fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`).then(res => res.json() )
-        }
-
-        Promise.all(storyPromises)
-            .then((data) => {setStories(data.title)  });
+        .then((data) => {
+                const newData = data.slice(0,20);
+                const promises = newData.map((storyId) => {
+                    return fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+                    .then(res => res.json() )
+                });
         
-
-    }
-
-
+                Promise.all(promises)
+                    .then((data) => {
+                        setStories(data);
+                    });
+                
+                })
+    }, [])
 
     // console.log("this is stories");
     // console.log(stories);
     return (
         <div>
             <h1>Stories</h1>
-            <StoryList stories={stories}/>
+            {/* <StoryList stories={stories}/> */}
         </div>
     )
 }
 
-export default MainContainer
+export default MainContainer;
+
+// I've commented out the <StoryList line at the bottom cos something in there is breaking it. But the 2nd level of data is being pulled and you can see it  in the state.
+
+
+
+
 
